3f0af04fd5d1cfc61e6320465c757b2cc0118441
Whirlatron <replit@whirlatron.anonaddy.com>
2020-08-09 17:29:46 +0000

    Reorganized code and added the ability to exit the program.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import random
 
+def generateproblem():
+    a = random.randint(0, 10)
+    b = random.randint(0, 10)
+    
+    p = '%i * %i' % (a, b)
+    s = a * b
+    return (p, s)
+
 def main():
     while True:
-        a = random.randint(0, 10)
-        b = random.randint(0, 10)
-        s = a * b
-        print('%i * %i' % (a, b))
+        p, s = generateproblem()
+        print(p)
         i = input('>>>')
-        if int(i) == s:
-            print('Correct!')
-        else:
-            print('WRONG!')
+        if i.lower() in ('stop', 'exit'):
+            break
+        try:
+            if int(i) == s:
+                print('Correct!')
+            else:
+                print('WRONG!')
+        except ValueError:
+            print('Invalid answer.')
 
 if __name__ == '__main__':
     main()
