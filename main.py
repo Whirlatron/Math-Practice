bed2755fcc72be781d3fe1f3a84973a895c06e88
Whirlatron <replit@whirlatron.anonaddy.com>
2020-08-09 19:22:49 +0000

    Added timing and statistics.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def generateproblem():
     a = random.randint(0, 10)
@@ -9,6 +10,9 @@ def generateproblem():
     return (p, s)
 
 def main():
+    probs = 0
+    cprobs = 0
+    start = time.time()
     while True:
         p, s = generateproblem()
         print(p)
@@ -18,10 +22,23 @@ def main():
         try:
             if int(i) == s:
                 print('Correct!')
+                cprobs += 1
             else:
-                print('WRONG!')
+                print('Wrong!')
         except ValueError:
             print('Invalid answer.')
+        probs += 1
+    end = time.time()
+
+    elapsed = round(end-start, 2)
+    percent = round(cprobs / probs * 100, 2)
+    persec = round(probs / elapsed, 2)
+
+    print(f'You got {cprobs} problems right out of {probs}')
+    print(f'Your accuracy was {percent}%')
+    print(f'You practiced for {elapsed} seconds.')
+    print(f'You completed {persec} problems per second')
+
 
 if __name__ == '__main__':
     main()
